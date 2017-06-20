3f27c12837ec294cb76d008f38c18f88fd4ad390
John Hughes <john@castlegateit.co.uk>
2017-06-20 09:27:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Template template
+
+A template for static templates. Note that `package.json` uses `*` for dependency versions, which will download the latest available versions. You may want to switch to something like `^2.1.x` to ensure you only get versions compatible with, for example, 2.1.x. See [documentation](https://docs.npmjs.com/files/package.json#dependencies).
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var maps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var prefix = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+var del = require('del');
+
+var src = './src';
+var dst = '.';
+
+gulp.task('clean:css', function () {
+    return del(dst + '/css/*');
+});
+
+gulp.task('clean:js', function () {
+    return del(dst + '/js/*');
+});
+
+gulp.task('compile:css', ['clean:css'], function () {
+    return gulp.src(src + '/scss/**/*.scss')
+        .pipe(maps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(prefix({browsers: ['Android 4', 'Explorer 9', 'Safari 7']}))
+        .pipe(maps.write('.'))
+        .pipe(gulp.dest(dst + '/css'));
+});
+
+gulp.task('compile:js', ['clean:js'], function () {
+    return gulp.src([
+        src + '/js/lib/jquery.js',
+        src + '/js/lib/**/*.js',
+        src + '/js/settings.js',
+        src + '/js/**/*.js'
+    ])
+        .pipe(plumber())
+        .pipe(maps.init())
+        .pipe(concat('script.js'))
+        .pipe(uglify({compress: false, output: {beautify: true}}))
+        .pipe(maps.write('.'))
+        .pipe(gulp.dest(dst + '/js'));
+});
+
+gulp.task('watch', ['default'], function () {
+    gulp.watch(src + '/scss/**/*.scss', ['compile:css']);
+    gulp.watch(src + '/js/**/*.js', ['compile:js']);
+});
+
+gulp.task('default', ['compile:css', 'compile:js']);
