bb61d6ed1feb151e81b1a6e143911625cee4d413
John Hughes <john@castlegateit.co.uk>
2019-02-08 14:30:22 +0000

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,24 +11,24 @@ var del = require('del');
 var src = './src';
 var dst = '.';
 
-gulp.task('clean:css', function () {
+gulp.task('clean:css', gulp.series(function () {
     return del(dst + '/css/*');
-});
+}));
 
-gulp.task('clean:js', function () {
+gulp.task('clean:js', gulp.series(function () {
     return del(dst + '/js/*');
-});
+}));
 
-gulp.task('compile:css', ['clean:css'], function () {
+gulp.task('compile:css', gulp.series(['clean:css'], function () {
     return gulp.src(src + '/scss/**/*.scss')
         .pipe(maps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(prefix({browsers: ['Android 4', 'Explorer 9', 'Safari 7']}))
         .pipe(maps.write('.'))
         .pipe(gulp.dest(dst + '/css'));
-});
+}));
 
-gulp.task('compile:js', ['clean:js'], function () {
+gulp.task('compile:js', gulp.series(['clean:js'], function () {
     return gulp.src([
         src + '/js/lib/jquery.js',
         src + '/js/lib/jquery.!(*.*).js',
@@ -43,11 +43,11 @@ gulp.task('compile:js', ['clean:js'], function () {
         .pipe(uglify({compress: false, output: {beautify: true}}))
         .pipe(maps.write('.'))
         .pipe(gulp.dest(dst + '/js'));
-});
+}));
 
-gulp.task('watch', ['default'], function () {
-    gulp.watch(src + '/scss/**/*.scss', ['compile:css']);
-    gulp.watch(src + '/js/**/*.js', ['compile:js']);
-});
+gulp.task('default', gulp.series(['compile:css', 'compile:js']));
 
-gulp.task('default', ['compile:css', 'compile:js']);
+gulp.task('watch', gulp.series(['default'], function () {
+    gulp.watch(src + '/scss/**/*.scss', gulp.series(['compile:css']));
+    gulp.watch(src + '/js/**/*.js', gulp.series(['compile:js']));
+}));
